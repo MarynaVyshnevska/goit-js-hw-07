2a16fede63419b938e6805d6407ee0132f66be60
Maryna Vyshnevska <vyshnevska.maryna@gmail.com>
2022-10-23 00:30:39 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,78 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryConteiner = document.querySelector('.gallery');
+const cardsGallery = createGalleryImg(galleryItems);
+
+galleryConteiner.insertAdjacentHTML('afterbegin', cardsGallery);
+
+galleryConteiner.addEventListener('click', onGalleryConteinerClick);
+
+
+
+// create html gallery
+function createGalleryImg(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+                <div class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                        />
+                    </a>
+                </div>
+            `;
+        })
+        .join('');
+}
+
+
+function onGalleryConteinerClick(e) {
+    // запрет на скачивание
+    e.preventDefault();
+
+    // что б не кликалось на картинку + modal
+    if (e.target.nodeName !== "IMG") {
+        return;
+        } else {
+        const instance = basicLightbox.create(`
+            <div class="modal">
+                <img src="${e.target.dataset.source}" width="1100" height="800">
+            </div>`
+            )
+            instance.show();  
+            console.log(e.target.dataset.source);
+            window.addEventListener("keydown", (e) => {
+                if (e.code === "Escape" || "Enter") {
+                    instance.close();
+                }
+            });
+        } 
+
+    changeActiveItemClass();
+    
+    const imgItem = e.target;
+    const parentGalleryItem = imgItem.closest('.gallery__item');
+    addActiveItemClass(parentGalleryItem);
+
+    // console.log(parentGalleryItem);
+    
+}
+
+//  снимаем класс с активного
+function changeActiveItemClass() {
+    const currentActiveItem = document.querySelector('.gallery__item.is-active');
+
+    if (currentActiveItem) {
+        currentActiveItem.classList.remove('is-active');
+    }
+}
+// ставим на активный
+function addActiveItemClass(item) {
+    item.classList.add('is-active');
+}
+
